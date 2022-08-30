3806ce8ee9a9380a9626e59923760c917755723f
NetherMC <thelio.trinite@outlook.com>
2022-08-30 16:45:12 +0200

    refactor

--- src/config/AnswerConfig.js
+++ src/config/AnswerConfig.js
@@ -4,17 +4,17 @@ const readline = require("readline");
 const words = [];
 
 module.exports = {
-    async loadWords(){
-        const filestream = fs.createReadStream(path.join(__dirname, '../../words.txt'))
-        const rl = readline.createInterface({
-            input: filestream,
-            crlfDelay: Infinity
-        });
-        for await (const line of rl){
-            words.push(line);
-        }
-    },
-    getWords(){
-        return words;
+  async loadWords() {
+    const filestream = fs.createReadStream(path.join(__dirname, '../../words.txt'))
+    const rl = readline.createInterface({
+      input: filestream,
+      crlfDelay: Infinity
+    });
+    for await (const line of rl) {
+      words.push(line);
     }
+  },
+  getWords() {
+    return words;
+  }
 }
