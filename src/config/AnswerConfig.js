4ce7dd8d73b100d4d75b3e36abbc0e7ad485a156
Shineeeeeuh <thomasbar59@gmail.com>
2022-08-30 16:09:54 +0200

    Finish loadWords function

--- src/config/AnswerConfig.js
+++ src/config/AnswerConfig.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
 const path = require("path");
-const words = []
+const readline = require("readline");
+const words = [];
 
 module.exports = {
     async loadWords(){
         const filestream = fs.createReadStream(path.join(__dirname, '../../words.txt'))
-        
+        const rl = readline.createInterface({
+            input: filestream,
+            crlfDelay: Infinity
+        });
+        for await (const line of rl){
+            words.push(line);
+        }
     }
 }
